f8387bc77c19159e10e52a821ee823b3f729c692
siyabongamatsheme <Siyabonga.matsheme@inchsolution.co.za>
2025-01-13 11:47:22 +0200

    Add files via upload
    initial files from Mr Mgoza

---
+++ src/UploadSchedule.js
@@ -0,0 +1,36 @@
+﻿
+const log = document.querySelector(".event-log-contents");
+
+document.addEventListener("DOMContentloaded", () => {
+
+    console.log("DOM fully loaded and parsed");
+    const allswitch = document.querySelectorAll(".ixm-tabitem");
+    const allswitchList = document.querySelectorAll(".ixm-tabitem-content");
+
+
+})
+
+document.addEventListener("click", myFunction1);
+
+function myFunction1() {
+console.log("First function was executed! ");
+log.textContent += "Function was executed!\n";
+
+
+alert("Hello");
+const allswitch = document.querySelectorAll(".ixm-tabitem");
+const allswitchList = document.querySelectorAll(".ixm-tabitem-content");    
+
+allswitch.forEach((sw) => {
+    sw.addEventListener('click', () => {
+        allswitch.forEach((t) => t.classList.remove("ixm-tabitem-selected"))
+        sw.classList.add("ixm-tabitem-selected");    
+
+        allswitchList.forEach((tl) => {
+        tl.style.display = tl.id == sw.getAttribute("data-tab") ? "block" : "none";
+        })
+
+    })
+
+})
+}
